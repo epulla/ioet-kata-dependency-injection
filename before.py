bcdbcce4519bedef5a87f06597df818b1c9ab35b
Erick Pulla Zambrano <erickpulladsi@gmail.com>
2021-12-23 15:21:35 -0500

    initial files

---
+++ before.py
@@ -0,0 +1,29 @@
+import string
+import random
+
+class Authorizer_SMS:
+
+    def __init__(self):
+        self.authorized = False
+        self.code = None
+
+    def generate_sms_code(self):
+        self.code = ''.join(random.choices(string.digits, k=6))
+
+    def authorize(self):
+        code = input("Enter SMS code: ")
+        self.authorized = code == self.code
+
+    def is_authorized(self) -> bool:
+        return self.authorized
+
+class PaymentProcessor:
+    
+    def pay(self, order):
+        authorizer = Authorizer_SMS()
+        authorizer.generate_sms_code()
+        authorizer.authorize()
+        if not authorizer.is_authorized():
+            raise Exception("Not authorized")
+        print(f"Processing payment for order with id {order.id}")
+        order.set_status("paid")
